fabc0ac6c2e09b7fed2399f253a7a734b24bdfe9
CHAN Hok Him <54910920+hhchanal@users.noreply.github.com>
2021-03-20 10:42:33 +0800

    fixed deduplicate function

--- server/service/cartSvc.js
+++ server/service/cartSvc.js
@@ -1,5 +1,18 @@
 let deduplicate = (rows) => {
     //Deduplicate logic
+    rows.forEach(function (value, index, arr) {
+    	value.quantity = 0;
+    	let id = value.id;
+    	for (let i = 0; i < rows.length; i++) {
+    		if (value.id == rows[i].id) {
+    			value.quantity++;
+    			if (index != i) {
+    				rows.splice(i, 1);
+    				i--;
+    			}
+    		}
+    	}
+    });
     return rows;
 };
 
